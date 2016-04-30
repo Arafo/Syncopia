287077f147a41a2b5bb546f082ca0acdf61d987b
Arafo <rafa_marcen@hotmail.com>
2016-04-30 17:45:35 +0200

    Añadida una primera versión del menu principal

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartRace() {
+        SceneManager.LoadScene("Test");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
