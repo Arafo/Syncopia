91177a0799eb60dd60611a500333f3dd47a87458
Arafo <rafa_marcen@hotmail.com>
2016-10-17 19:28:03 +0200

    Botón horizontal modificado para que funcione con mandos y teclados

--- Assets/Scripts/UI/HorizontallScrollSliderButton.cs
+++ Assets/Scripts/UI/HorizontallScrollSliderButton.cs
@@ -2,16 +2,45 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
+//using TeamUtility.IO;
+using Rewired;
+using UnityEngine.EventSystems;
 
 public class HorizontallScrollSliderButton : Button {
 
     public MenuEventManager menuManager;
     public HorizontallScrollSlider button;
+    private float m_HorizontalInput;
+    private bool m_Highlighted = false;
+    private float m_InputSteep;
+    private float m_MaxInputSteep = 0.5f;
+
 
     // Update is called once per frame
     void Update () {
-	
-	}
+        m_HorizontalInput = ReInput.players.GetPlayer(0).GetAxis("MenuHorizontal");
+        if (m_Highlighted) {
+            m_InputSteep += Time.deltaTime;
+            if (m_HorizontalInput > 0 && m_InputSteep > m_MaxInputSteep) {
+                button.setNextText();
+                m_InputSteep = 0f;
+            }
+            else if (m_HorizontalInput < 0 && m_InputSteep > m_MaxInputSteep) {
+                button.setLastText();
+                m_InputSteep = 0f;
+            }
+        }
+    }
+
+    public override void OnSelect(BaseEventData data) {
+        base.OnSelect(data);
+        m_Highlighted = true;
+    }
+
+    public override void OnDeselect(BaseEventData eventData) {
+        base.OnDeselect(eventData);
+        m_Highlighted = false;
+    }
 
     /// <summary>
     /// 
